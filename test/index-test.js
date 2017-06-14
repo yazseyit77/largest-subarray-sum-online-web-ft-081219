92fcaf7edb370d9f420906be06f92411e85317fd
Zac Baston <zbaston@gmail.com>
2017-06-14 13:49:17 -0400

    fix typo

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
 describe('#largestSubarraySum', function() {
-	it('return largest subarray sumc', function(){
+	it('return largest subarray sum', function(){
 		let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
 		expect(largestSubarraySum(array)).toEqual(16)
 	})
