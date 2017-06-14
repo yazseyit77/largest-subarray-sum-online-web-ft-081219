62e63301130f4028e11322e1f181aaa9390594c9
Zac Baston <zbaston@gmail.com>
2017-06-14 13:48:22 -0400

    add more tests to handle weird cases

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,21 @@
 describe('#largestSubarraySum', function() {
-	it('largest subarray sum', function(){
+	it('return largest subarray sumc', function(){
 		let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
 		expect(largestSubarraySum(array)).toEqual(16)
+	})
+
+	it('returns largest subarray sum', function(){
+		let array = [-2, -3, 4, -1, -2, 1, 5, -3]
+		expect(largestSubarraySum(array)).toEqual(7)
+	})
+
+	it('returns largest subarray sum', function(){
+		let array = [-2, -5, 6, -2, -3, 1, 5, -6]
+		expect(largestSubarraySum(array)).toEqual(7)
+	})
+
+	it('returns largest subarray sum', function(){
+		let array = [2, 1, -3, 4, -1, 2, 1, -5, 4]
+		expect(largestSubarraySum(array)).toEqual(6)
 	})
 });
