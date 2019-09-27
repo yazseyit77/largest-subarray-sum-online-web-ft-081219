f23e6da4817fb0766f33b7c2749af2d5a335dc59
Rob Cobb <rwcobbjr@gmail.com>
2019-09-27 14:24:40 -0400

    add O(n) solution with explanatory comments

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 function largestSubarraySum(array) {
-  // naive n**2 implementation
+  // O(n) solution
+  // uses the fact that any time the running total goes below zero,
+  // we should reset the sum to 0, since the values 'behind' the index
+  // are 'hidden' by negative numbers
+  let sum = 0;
   let max = 0;
-  for(let start = 0; start < array.length; start++) {
-    for (let end = start; end < array.length+1; end++) {
-      let subarray = array.slice(start, end)
-      let sum = subarray.reduce((memo, item) => memo + item, 0);
-      max = Math.max(max, sum);
+
+  array.forEach(value => {
+    if (sum + value < 0) {
+      // handle case where value is a larger negative than the running total
+      // i.e. restart the sum at 0, indicating a restart of the subarray
+      sum = 0;
+    } else {
+      sum += value
+      if (sum > max) {
+        max = sum
+      }
     }
-  }
+  })
+
   return max;
 }
