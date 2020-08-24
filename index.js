30a24886ed075504fcb8f70b1acc8072795b6a96
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-24 23:49:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function largestSubarraySum(arr){
+  let largestSum = 0;
+  let sum = arr[0];
+
+  for(let i = 1; i < arr.length; i++){
+    sum = Math.max(arr[i], sum + arr[i])
+    if(largestSum < sum){
+      largestSum = sum;
+    }
+  }
+}
